2bf6c3edc99a626b856b80d4b23f1385b48bcc0b
Jonathan Jackson <jonathan.jackson1@me.com>
2016-06-14 20:25:29 -0400

    Add connectivity message on offline "older tils" link

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -2,19 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   page: 1,
+  flashMessages: Ember.inject.service(),
   model() {
     return this.store.findAll('post');
   },
-  setupController(controller, model) {
-    controller.setProperties({
-      model: model,
-      posts: model
-    })
-  },
   actions: {
     showMore() {
-      const page = this.incrementProperty('page');
-      this.store.query('post', { page });
+      this.store.query('post', { page: this.get('page') }).then(() => {
+	this.incrementProperty('page');
+      }).catch(() => {
+	this.get('flashMessages').info('Connectivity derailed');
+      });
     }
   }
 });
